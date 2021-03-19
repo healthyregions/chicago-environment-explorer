d8b020258e6d7a3c6613fe9346d41280d44f65ef
Dylan <d_h@mit.edu>
2021-03-19 15:15:56 -0500

    build fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,11 @@
 import dataFn from './dataFunction';
 import getVarId from './getVarId';
 import mapFn from './mapFunction';
-import getURLParams from './getUrlParams';
+// import getURLParams from './getUrlParams';
 
 export {
     dataFn,
     getVarId,
     mapFn,
-    getURLParams
+    // getURLParams
 }
