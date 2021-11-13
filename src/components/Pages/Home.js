392741e44e8b2c8ddaee8f8a398f58faa8d5b10d
Marynia <marynia.kolak@gmail.com>
2021-11-12 21:24:41 -0600

    home page
    updated front page content & some scaffolding pieces

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -7,6 +7,8 @@ import Grid from '@material-ui/core/Grid';
 import { StaticNavbar, Footer } from '../../components';
 import { colors } from '../../config';
 import { Gutter } from '../../styled_components';
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+import onion from './onion.png'
 
 
 const HomePage = styled.div`
@@ -37,6 +39,13 @@ const HomePage = styled.div`
         line-height: 1.5;
         color: ${colors.darkgray};
     }
+     .photo {
+      width: 100%;
+
+    }
+    a {
+        color: ${colors.chicagoBlue};
+    }
 `
 
 const HomePageContent = styled.div`
@@ -63,19 +72,35 @@ const Hero = styled.div`
         padding:2rem 0;
     }
     #button-cta {
+        font-family: 'Lora', serif;
+        font-size: 20px;
+        font-weight: 700;
+        letter-spacing: 1.75px;
+        line-height:5;
+        text-align: center;
+        background-color: #FFFFFF ;
+        color: ${colors.darkgray};
+        padding: 1rem 1.5rem;
+        margin:1rem;
+        // border-radius: .3rem;
+        text-decoration:none;
+    }
+
+    #button-search{
         font-size: 16px;
         font-weight: 700;
         letter-spacing: 1.75px;
         line-height:5;
         text-align: center;
         text-transform:uppercase;
-        background-color: ${colors.chicagoBlue};
-        color: #0d0d0d;
+        background-color: ${colors.darkgray};
+        color: #FFFFFF;
         padding: 1rem 1.5rem;
         margin:1rem;
         // border-radius: .3rem;
         text-decoration:none;
     }
+
     .small-text {
         font-size:0.75rem;
         a {
@@ -98,51 +123,57 @@ const Hero = styled.div`
 `
 
 
-
-
-
-
 export default function Home(){
     return (
        <HomePage>
            <StaticNavbar/>
            <HomePageContent>
                 <Hero>
-                    <h1>Uncover the nature of Chicago's environment.</h1>
-                    <p>
-                        Chicago's urban environment has profound impacts on the health of communities and individuals. 
-                        A handful of key metrics―tree cover, air pollution estimates, heat island effects, traffic volumes, and social vulnerability index―
-                        help to reveal where in the city people face particular challenges as we work towards a healthier Chicago.
-                        <br/><br/>
 
-                        Like the <a href="">smelly onion</a> that Chicago was named for, data about the city's environment should 
-                        be <b>tasty</b> and <b>plentiful</b>. By tasty, we mean easily accessible and ready to use and explore. By plentiful,
-                        we mean the data should extend across multiple dimensions the city's landscape, and be updated regularly.<br/><br/>
+                            <h1>Uncover the nature of Chicago's environment.</h1>
+                            <p>
+                                Chicago's urban environment has profound impacts on the health of communities and individuals. 
+                                A handful of key metrics―tree cover, air pollution estimates, heat island effects, traffic volumes, and social vulnerability index―
+                                help to reveal where in the city people face particular challenges as we work towards a healthier Chicago.
+                            </p>
 
-                    </p>
-
-                    <NavLink to="/map" id="button-cta">Explore Chicago's Environment</NavLink><br/>
-                    <br/><br/>
+                                <br/> 
+                                <NavLink to="/map" id="button-cta">Explore Chicago's Environment --> </NavLink>
+                                <NavLink to="/map" id="button-search">Enter an Address to Start</NavLink><br/>
 
 
-                    <p>    
 
-                        This project refactors data from the a <a href="">previous partnership</a> with the City, and gets customized
-                         as a new platform for the wider community. We invite to you add more resources to the CHIVES explorer as build out
-                         an open data coalition to prepare Chicago for a more resilient future!
 
-                    </p>
 
                             
-                    <Grid container spacing ={2}>
+                <Grid container spacing ={2}>
                         <Grid item xs={4}> 
-                            <Grid item> Text </Grid>
+                            <Grid item> 
+                                <img className="photo" src={onion} alt="Wild Onion" loading="lazy"/>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={4}> 
-                            <Grid item> ... </Grid>
-                        </Grid>
-                        <Grid item xs={4}> 
-                            <Grid item> Text </Grid>
+                        <Grid item xs={8}> 
+                            <Grid item> 
+
+                    <br/><br/>
+
+                            <p> Like the <a href="">wild onion</a> that Chicago was named for, data about the city's environment should 
+                        be <b>tasty</b> and <b>plentiful</b>. By tasty, we mean easily accessible and ready to use and explore. By plentiful,
+                        we mean the data should extend across multiple dimensions of the city's landscape, and be updated regularly. 
+                            <br/><br/>
+                        We <a href="">harmonize & standardize</a> environmental data across dozens of sources to make it accessible for full exploration, alongside a growing list of 
+                        resources on the Chicago Environment, cultivated by a community of curators.
+                            <br/><br/>
+                            This project refactors data from the a <a href="">previous partnership</a> with the City, and gets customized
+                        as a new opensource platform for the wider community. <br/><br/>
+
+                            Have data to add? A mapping resource to recommend? <a href="">We invite you</a> to add more resources to the <i>ChiVes</i> explorer as build out
+                        an open data coalition to prepare Chicago for a more resilient future! </p>
+
+                           <h5>Image of <i>Wild Onion, Allium acuminatum </i> by Margaret Neilson Armstrong (1913) via <br/> 
+                           Creative Commons CC0 1.0 Universal Public Domain Dedication.</h5>
+
+                            </Grid>
                         </Grid>
                     </Grid>
 
@@ -152,8 +183,8 @@ export default function Home(){
 
 
 
-
                 </Hero>
+
 
 
 
