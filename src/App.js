b3b73dc60d11fd2db5ba9d9f1ef6ac72f5b288c1
theuscovidatlas <dhalpern@uchicago.edu>
2021-11-18 09:28:23 -0600

    Implement GoogleAnalytics
    *** Note- new packages added (history and react-ga). Please run yarn or npm install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component, Suspense  } from 'react';
+import React, { useEffect, Suspense  } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
  
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from "history";
 import {Home, LoadingMessage, ErrorPage} from './components/';
  
 const Map = React.lazy(() => import('./components/Pages/Map'));
@@ -8,31 +10,38 @@ const About = React.lazy(() => import('./components/Pages/About'));
 const Contact = React.lazy(() => import('./components/Pages/Contact'));
 const Data = React.lazy(() => import('./components/Pages/Data'));
 
-class App extends Component {
+export default function App() {
+    const history = createBrowserHistory();
 
-  render() {
+    history.listen(location => {
+      ReactGA.initialize('G-K0HGLZLGXG');
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    });
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    }, [history])
+
     return (     
-       <Router basename={process.env.PUBLIC_URL}>
-        <div>
-          <Suspense fallback={<LoadingMessage />}>
-              <Switch>
-                <Route path="/" component={Home} exact/>
-                <Route path="/map" component={Map}/>
-                <Route path="/map.html" component={Map}/>
-                <Route path="/about" component={About}/>
-                <Route path="/about.html" component={About}/>
-                <Route path="/contact" component={Contact}/>
-                <Route path="/contact.html" component={Contact}/>
-                <Route path="/data" component={Data}/>
-                <Route path="/data.html" component={Data}/>
-                <Route component={ErrorPage} />
-                <Route />
-            </Switch>
-          </Suspense>
-        </div> 
-      </Router>
-    );
-  }
+      <Router basename={process.env.PUBLIC_URL} history={history}>
+      <div>
+        <Suspense fallback={<LoadingMessage />}>
+            <Switch>
+              <Route path="/" component={Home} exact/>
+              <Route path="/map" component={Map}/>
+              <Route path="/map.html" component={Map}/>
+              <Route path="/about" component={About}/>
+              <Route path="/about.html" component={About}/>
+              <Route path="/contact" component={Contact}/>
+              <Route path="/contact.html" component={Contact}/>
+              <Route path="/data" component={Data}/>
+              <Route path="/data.html" component={Data}/>
+              <Route component={ErrorPage} />
+              <Route />
+          </Switch>
+        </Suspense>
+      </div> 
+    </Router>
+  );
 }
-
-export default App
