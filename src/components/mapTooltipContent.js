43eb6f3b9e6d9c4b3a08f3b379c9e388fbaeee65
Marynia <marynia.kolak@gmail.com>
2021-11-26 20:33:31 -0600

    Update mapTooltipContent.js

--- src/components/mapTooltipContent.js
+++ src/components/mapTooltipContent.js
@@ -8,7 +8,7 @@ const MapTooltipContent = (props) => {
                 <tr><td>Population</td><td> {props.content.acs_population && props.content.acs_population.toLocaleString('en')}</td></tr>
                 <tr><td>Number of Trees</td><td> {props.content.trees_n && props.content.trees_n.toLocaleString('en')}</td></tr>
                 <tr><td>Percent Canopy Cover</td><td> {props.content.trees_crown_den && props.content.trees_crown_den.toFixed(2)}%</td></tr>
-                <tr><td>Heat Island Percentile</td><td> {props.content.heatisl && props.content.heatisl.toFixed(2)}</td></tr>
+                <tr><td>Surface Temp. Percentile</td><td> {props.content.heatisl && props.content.heatisl.toFixed(2)}</td></tr>
                 <tr><td>PM2.5 in Summer (Model Average)</td><td> {props.content.nn_q3_pm2_5 && props.content.nn_q3_pm2_5.toFixed(2)}</td></tr>
                 <tr><td>Urban Flood Susceptibility Index</td><td> {props.content.urban_flood_suscep && props.content.urban_flood_suscep.toFixed(2)}</td></tr>
                 <tr><td>Logged Traffic Volume</td><td> {props.content.logtraf && props.content.logtraf.toFixed(2)}</td></tr>
