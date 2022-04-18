be2f73054027f8433b877ec3ccaae503d2e6ace8
Dylan <dhalpern@uchicago.edu>
2022-04-18 17:14:16 -0500

    Start three up grid

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,165 +1,223 @@
-import React, { useCallback } from 'react';
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useCallback } from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 
-import { Geocoder, NavBar, Footer } from '../../components';
-import { colors } from '../../config';
-import { Gutter } from '../../styled_components';
+import { Geocoder, NavBar, Footer } from "../../components";
+import { colors } from "../../config";
+import { Gutter } from "../../styled_components";
 // import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
-import onion from './onion.png'
+import onion from "./onion.png";
 
 const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const HomePage = styled.div`
-    h1 {    
-        font-family: 'Lora', serif;
-        font-size: 4rem;
-        font-weight: 300;
-        text-align: left;
-        color: ${colors.black};
-        width: 80vw;
-        max-width: 940px;
-        margin: 0 0 40px 0;
+  h1 {
+    font-family: "Lora", serif;
+    font-size: 4rem;
+    font-weight: 300;
+    text-align: left;
+    color: ${colors.black};
+    width: 80vw;
+    max-width: 940px;
+    margin: 0 0 40px 0;
+  }
+  .h1,
+  .h2,
+  .h3,
+  .h4,
+  .h5,
+  .h6,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    line-height: 1.2;
+  }
+  hr {
+    max-width: 1140px;
+    margin: 6em auto;
+    border: 0;
+    border-top: 1px solid ${colors.darkgray};
+  }
+  p {
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: ${colors.darkgray};
+  }
+  .photo {
+    width: 100%;
+    @media (max-width: 960px) {
+      max-height: 40vh;
+      width: auto;
+      margin: 0 auto;
+      display: block;
     }
-    .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
-        margin-bottom: .5rem;
-        font-weight: 500;
-        line-height: 1.2;
-    }
-    hr {
-        max-width:1140px;
-        margin:6em auto;
-        border: 0;
-        border-top: 1px solid ${colors.darkgray};
-    } 
-    p {
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 1.5;
-        color: ${colors.darkgray};
-    }
-    .photo {
-        width: 100%;
-        @media(max-width: 960px) {
-            max-height:40vh;
-            width: auto;
-            margin:0 auto;
-            display:block;
-        }
-    }
-    a {
-        color: ${colors.chicagoBlue};
-    }
-`
+  }
+  a {
+    color: ${colors.chicagoBlue};
+  }
+`;
 
 const HomePageContent = styled.div`
-    width:100%;
-    margin:0 auto;
-`
+  width: 100%;
+  margin: 0 auto;
+`;
 
 const Hero = styled.div`
-    width:100%;
-    max-width:1140px;
-    text-align:center;
-    color: ${colors.lightgray};
-    margin:0 auto;
-    padding:50px 10px 0 10px;
-    p {
-        
-        font-family: 'Lora', sans-serif;
-        font-size: 1.25rem;
-        font-stretch: normal;
-        text-align:left;
-        font-style: normal;
-        line-height: 1.6;
-        letter-spacing: normal;
-        padding:2rem 0;
+  width: 100%;
+  max-width: 1140px;
+  text-align: center;
+  color: ${colors.lightgray};
+  margin: 0 auto;
+  padding: 50px 10px 0 10px;
+  p {
+    font-family: "Lora", sans-serif;
+    font-size: 1.25rem;
+    font-stretch: normal;
+    text-align: left;
+    font-style: normal;
+    line-height: 1.6;
+    letter-spacing: normal;
+    padding: 2rem 0;
+  }
+  #button-cta {
+    font-family: "Lora", serif;
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 1.75px;
+    line-height: 5;
+    text-align: center;
+    background-color: #ffffff;
+    color: ${colors.darkgray};
+    padding: 1rem 1.5rem;
+    margin: 1rem;
+    // border-radius: .3rem;
+    text-decoration: none;
+  }
+
+  #button-search {
+    font-size: 16px;
+    font-weight: 700;
+    letter-spacing: 1.75px;
+    text-align: center;
+    text-transform: uppercase;
+    background-color: ${colors.darkgray};
+    color: #ffffff;
+    padding: 1rem 1.5rem;
+    text-decoration: none;
+    line-height: 2.5;
+  }
+
+  .small-text {
+    font-size: 0.75rem;
+    a {
+      font-size: 0.75rem;
+      color: ${colors.orange};
+      text-decoration: none;
     }
-    #button-cta {
+  }
+  video {
+    margin-bottom: 20px;
+    width: 100%;
+    max-width: 600px;
+  }
+  .map-caption {
+    font-size: 0.9rem;
+    text-align: left;
+  }
+`;
+
+const ThreeUpGrid = styled(Grid)`
+    background: ${colors.forest};
+    padding: 2em 1em;
+    margin: 4em 0;
+    color:white;
+    border-radius:1em;
+    h2 {
+        font-size: 1.5rem;
         font-family: 'Lora', serif;
-        font-size: 20px;
-        font-weight: 700;
-        letter-spacing: 1.75px;
-        line-height:5;
-        text-align: center;
-        background-color: #FFFFFF ;
-        color: ${colors.darkgray};
-        padding: 1rem 1.5rem;
-        margin:1rem;
-        // border-radius: .3rem;
-        text-decoration:none;
     }
-
-    #button-search{
-        font-size: 16px;
-        font-weight: 700;
-        letter-spacing: 1.75px;
-        text-align: center;
-        text-transform:uppercase;
-        background-color: ${colors.darkgray};
-        color: #FFFFFF;
-        padding: 1rem 1.5rem;
-        text-decoration:none;
-        line-height:2.5;        
+    p {
+        color:white;
+        font-family: 'Montserrat', sans-serif;
     }
-
-    .small-text {
-        font-size:0.75rem;
-        a {
-            font-size:0.75rem;
-            color:${colors.orange};
-            text-decoration:none;
-        }
-    }
-    video {
-        margin-bottom:20px;
-        width:100%;
-        max-width:600px;
-    }
-    .map-caption {
-        font-size:0.9rem;
-        text-align:left;
-
-    }
-
 `
 
-export default function Home(){
-        
-    const handleGeocoder = useCallback(location => {
-        if (location.center !== undefined) {
-            let url = '';
-            
-            if ((`${window.location.href}`).includes('index')) {
-                url += (`${window.location.href}`).split('index')[0]
-            } else {
-                url += window.location.href
-            }
-            url += `map?lat=${location.center[1]}&lon=${location.center[0]}`
-            window.location.href = url 
-        }  
-    }, []);
+export default function Home() {
+  const handleGeocoder = useCallback((location) => {
+    if (location.center !== undefined) {
+      let url = "";
 
-    return (
-       <HomePage>
-           <NavBar />
-           <HomePageContent>
-                <Hero>
-                    <h1>Uncover the nature of Chicago's environment.</h1>
-                    <p>
-                        Chicago's urban environment has profound impacts on the health of communities and individuals. 
-                        A handful of key metrics―tree cover, air pollution estimates, heat island effects, traffic volumes, and social vulnerability index―
-                        help to reveal where in the city people face particular challenges as we work towards a healthier Chicago.
-                    </p>
-                    <br/> 
-                    
-                    <NavLink to="/" id="button-search">Chives: Explore The Chicago Environment in April 2022</NavLink>
-                    <Gutter h={40}/>
-                    
+      if (`${window.location.href}`.includes("index")) {
+        url += `${window.location.href}`.split("index")[0];
+      } else {
+        url += window.location.href;
+      }
+      url += `map?lat=${location.center[1]}&lon=${location.center[0]}`;
+      window.location.href = url;
+    }
+  }, []);
 
-                    {/* <Grid container spacing={8} alignItems="center">
+  return (
+    <HomePage>
+      <NavBar />
+      <HomePageContent>
+        <Hero>
+          <h1>Uncover the nature of Chicago's environment.</h1>
+          <p>
+            Chicago's urban environment has profound impacts on the health of
+            communities and individuals. A handful of key metrics―tree cover,
+            air pollution estimates, heat island effects, traffic volumes, and
+            social vulnerability index― help to reveal where in the city people
+            face particular challenges as we work towards a healthier Chicago.
+          </p>
+          <br />
+
+          <NavLink to="/map" id="button-search">
+            Chives: Explore The Chicago Environment
+          </NavLink>
+          <Gutter h={40} />
+          <Grid container spacing={8} alignItems="center">
+            <Grid item xs={12} sm={12} md={6}>
+              <p>
+                <i>
+                  Got somewhere you want to explore? Search for an address to
+                  navigate directly to your neighborhood.
+                </i>
+              </p>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <Geocoder
+                id="Geocoder"
+                placeholder={"Find your location - eg. 1155 E 60th Street"}
+                API_KEY={MAPBOX_ACCESS_TOKEN}
+                onChange={handleGeocoder}
+              />
+            </Grid>
+          </Grid>
+          <ThreeUpGrid container spacing={1}>
+            <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                <h2>Community</h2>
+                <p>Description</p>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                <h2>Open Data</h2>
+                <p>Description</p>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                <h2>ChiVes Collaborative</h2>
+                <p>Description</p>
+            </Grid>
+        </ThreeUpGrid>
+
+          {/* <Grid container spacing={8} alignItems="center">
                         <Grid item xs={12} sm={12} md={6}>
                             <p>
                                 <i>Got somewhere you want to explore? Search for an address to navigate directly to your neighborhood.</i>
@@ -174,39 +232,57 @@ export default function Home(){
                             />
                         </Grid>
                     </Grid> */}
-                    
 
-
-                    <Gutter h={20}/>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={4}> 
-                            <img className="photo" src={onion} alt="Wild Onion" loading="lazy"/>
-                        </Grid>
-                        <Grid item xs={12} md={8}> 
-                            <br/><br/>
-                            <p> 
-                                Like the <a href="https://www.illinoiswildflowers.info/woodland/plants/wild_leek.htm">wild onion</a> that Chicago was named for, data about the city's environment should 
-                                be <b>tasty</b> and <b>plentiful</b>. By tasty, we mean easily accessible and ready to use and explore. By plentiful,
-                                we mean the data should extend across multiple dimensions of the city's landscape, and be updated regularly. 
-                                <br/><br/>
-                                We <a href="/data">harmonize & standardize</a> environmental data across dozens of sources to make it accessible for full exploration, alongside a growing list of 
-                                resources on the Chicago Environment, cultivated by a community of curators.
-                                <br/><br/>
-                                ChiVes is a data collaborative and community mapping application that brings data on Chicago’s environment together at the neighborhood level. 
-                                Have data to add? A mapping resource to recommend? Learn <a href="/about">more</a>!
-                            </p>
-                            <h5>
-                                Image of <i>Wild Onion, Allium acuminatum </i> by Margaret Neilson Armstrong (1913) via 
-                                <br/> 
-                                Creative Commons CC0 1.0 Universal Public Domain Dedication.
-                            </h5>
-                        </Grid>
-                    </Grid>
-                    <br/><br/>
-                    <Gutter h={20}/>
-                </Hero>
-           </HomePageContent>
-           <Footer signUp={false} />
-       </HomePage>
-    );
+          <Gutter h={20} />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <img
+                className="photo"
+                src={onion}
+                alt="Wild Onion"
+                loading="lazy"
+              />
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <br />
+              <br />
+              <p>
+                Like the{" "}
+                <a href="https://www.illinoiswildflowers.info/woodland/plants/wild_leek.htm">
+                  wild onion
+                </a>{" "}
+                that Chicago was named for, data about the city's environment
+                should be <b>tasty</b> and <b>plentiful</b>. By tasty, we mean
+                easily accessible and ready to use and explore. By plentiful, we
+                mean the data should extend across multiple dimensions of the
+                city's landscape, and be updated regularly.
+                <br />
+                <br />
+                We <a href="/data">harmonize & standardize</a> environmental
+                data across dozens of sources to make it accessible for full
+                exploration, alongside a growing list of resources on the
+                Chicago Environment, cultivated by a community of curators.
+                <br />
+                <br />
+                ChiVes is a data collaborative and community mapping application
+                that brings data on Chicago’s environment together at the
+                neighborhood level. Have data to add? A mapping resource to
+                recommend? Learn <a href="/about">more</a>!
+              </p>
+              <h5>
+                Image of <i>Wild Onion, Allium acuminatum </i> by Margaret
+                Neilson Armstrong (1913) via
+                <br />
+                Creative Commons CC0 1.0 Universal Public Domain Dedication.
+              </h5>
+            </Grid>
+          </Grid>
+          <br />
+          <br />
+          <Gutter h={20} />
+        </Hero>
+      </HomePageContent>
+      <Footer signUp={false} />
+    </HomePage>
+  );
 }
