dad5a0f50c43b955dceb02b16366fcb9320b75b5
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-04-21 16:57:22 -0500

    Cleanup Tooltip and add data

--- src/components/mapTooltipContent.js
+++ src/components/mapTooltipContent.js
@@ -4,7 +4,21 @@ import {colors} from '../config'
 const PolarSpeciesPlot = React.lazy(() => import('../components/PolarSpeciesPlot.js'));
 // This component handles and formats the map tooltip info. 
 // The props passed to this component should contain an object of the hovered object (from deck, info.object by default)
-const MapTooltipContent = (props) => {
+const MapTooltipContent = ({content}) => {
+    const {
+        geoid,
+        acs_population,
+        trees_n,
+        trees_crown_den,
+        heatisl,
+        nn_q3_pm2_5,
+        logtraf,
+        svi_pecentile,
+        asthma_age_adj_rate, 
+        urban_flood_suscep,
+        ndvi,
+        simpson
+    } = content;
     const [speciesPlotInfo, setSpeciesPlotInfo] = useState({
         open: false,
         geoid: null
@@ -13,7 +27,7 @@ const MapTooltipContent = (props) => {
     const handleSpeciesPlot = () => {
         setSpeciesPlotInfo({
             open: true,
-            geoid: +props.content.geoid
+            geoid: +geoid
         });
     }
     const handleSetOpen = (bool) => {
@@ -25,18 +39,22 @@ const MapTooltipContent = (props) => {
 
     return (
         <>
-            <h2>Tract {props.content.geoid}</h2>
-            {props.content && <span><table>
-                <tr><td>Population</td><td> {props.content.acs_population && props.content.acs_population.toLocaleString('en')}</td></tr>
-                <tr><td>Number of Trees</td><td> {props.content.trees_n && props.content.trees_n.toLocaleString('en')}</td></tr>
-                <tr><td>Percent Canopy Cover</td><td> {props.content.trees_crown_den && props.content.trees_crown_den.toFixed(2)}%</td></tr>
-                <tr><td>Surface Temp Percentile</td><td> {props.content.heatisl && props.content.heatisl.toFixed(2)}</td></tr>
-                <tr><td>PM2.5 in Summer (Model Average)</td><td> {props.content.nn_q3_pm2_5 && props.content.nn_q3_pm2_5.toFixed(2)}</td></tr>
-                <tr><td>Urban Flood Susceptibility Index</td><td> {props.content.urban_flood_suscep && props.content.urban_flood_suscep.toFixed(2)}</td></tr>
-                <tr><td>Logged Traffic Volume</td><td> {props.content.logtraf && props.content.logtraf.toFixed(2)}</td></tr>
-                <tr><td>Social Vulnerability Index</td><td> {props.content.svi_pecentile && props.content.svi_pecentile.toFixed(2)}</td></tr>
-                <tr><td>Age Adjusted Asthma Cases (Per 10k)</td><td> {props.content.asthma_age_adj_rate && props.content.asthma_age_adj_rate}</td></tr>
-                <tr><td>Urban Flood Susceptibility Index</td><td> {props.content.urban_flood_suscep && props.content.urban_flood_suscep.toFixed(2)}</td></tr>
+            <h2>Tract {geoid}</h2>
+            {content && <span><table>
+                <tbody>
+                    <tr><td>Population</td><td> {acs_population && acs_population.toLocaleString('en')}</td></tr>
+                    <tr><td>Number of Trees</td><td> {trees_n && trees_n.toLocaleString('en')}</td></tr>
+                    <tr><td>Percent Canopy Cover</td><td> {trees_crown_den && trees_crown_den.toFixed(2)}%</td></tr>
+                    <tr><td>Surface Temp Percentile</td><td> {heatisl && heatisl.toFixed(2)}</td></tr>
+                    <tr><td>PM2.5 in Summer (Model Average)</td><td> {nn_q3_pm2_5 && nn_q3_pm2_5.toFixed(2)}</td></tr>
+                    <tr><td>Urban Flood Susceptibility Index</td><td> {urban_flood_suscep && urban_flood_suscep.toFixed(2)}</td></tr>
+                    <tr><td>Logged Traffic Volume</td><td> {logtraf && logtraf.toFixed(2)}</td></tr>
+                    <tr><td>Social Vulnerability Index</td><td> {svi_pecentile && svi_pecentile.toFixed(2)}</td></tr>
+                    <tr><td>Age Adjusted Asthma Cases (Per 10k)</td><td> {asthma_age_adj_rate && asthma_age_adj_rate}</td></tr>
+                    <tr><td>Urban Flood Susceptibility Index</td><td> {urban_flood_suscep && urban_flood_suscep.toFixed(2)}</td></tr>
+                    <tr><td>Vegetation Index (NDVI)</td><td> {ndvi && ndvi.toFixed(3)}</td></tr>
+                    <tr><td>Plant Biodiversity</td><td>{simpson && simpson.toFixed(2)}</td></tr>
+                </tbody>
             </table>
             <Button variant="contained" onClick={handleSpeciesPlot} style={{marginTop:'.5em', fontFamily:'"Lato", sans-serif', background:colors.forest}}>Open Species Tree</Button>
             <PolarSpeciesPlot
