e3602d922187b20d17123df1fa68808edb48ac06
Marynia <marynia.kolak@gmail.com>
2022-04-21 20:29:59 -0500

    update carousel showcase

--- src/config/showcase.js
+++ src/config/showcase.js
@@ -4,18 +4,36 @@ export const SHOWCASE_ITEMS = [
         text: 'PM2.5',
         image: BASE_URL + '/img/showcase/pm25.jpg',
         icon: BASE_URL + '/img/showcase/icons/pm25.png',
-        explainerText:"PM2.5 is a measure of..."
+        explainerText:"This approximation of PM2.5 air pollution, or particulate matter sized 2.5 microns or smaller, comes from a predictive model using ground and satellite sensors. Check out the summer average, and look forward to new datasets in coming months!"
     },
     {
         text: 'Vegetation',
-        image: BASE_URL + '/img/showcase/ndvi.jpg',
+        image: BASE_URL + '/img/showcase/ndvi2.png',
         icon: BASE_URL + '/img/showcase/icons/ndvi.png',
-        explainerText:"Vegetation index, or NDVI, is a measure of..."
+        explainerText:"How green is your community? The Vegetation Index, measured from satellite data, gives a measure of the abundance of vegetation for every tract in the City. Freshly made from DePaul University. Zoom in all the way to see individual trees!"
     },
     {
         text: 'Biodiversity',
         image: BASE_URL + '/img/showcase/biodiversity.jpg',
         icon: BASE_URL + '/img/showcase/icons/biodiversity.png',
-        explainerText:"Biodiversity data..."
+        explainerText:"The Botanic Gardens just released this dataset on Community Sampled Plant Diversity. Check out the biodiversity in your neighborhood, and then contribute to the data by adding plant species yourself!"
+    },
+    {
+        text: 'Traffic Volume',
+        image: BASE_URL + '/img/showcase/traffic.png',
+        icon: BASE_URL + '/img/showcase/icons/pm25.png',
+        explainerText:"How bad is traffic in your area? We developed a score of traffic volume based on traffic estimates form the Illinois Department of Transportation. Zoom into the map, tile (using your control key), and explore!"
+    },
+    {
+        text: 'Redlining',
+        image: BASE_URL + '/img/showcase/redlining.png',
+        icon: BASE_URL + '/img/showcase/icons/ndvi.png',
+        explainerText:"A legacy of redlining and racialized deinvestment is a part of Chicago's history. Overlay historic redlining areas to see how communities today may have been shaped from the past."
+    },
+    {
+        text: 'Asthma',
+        image: BASE_URL + '/img/showcase/asthma.png',
+        icon: BASE_URL + '/img/showcase/icons/biodiversity.png',
+        explainerText:"We connect community level health data from multiple sources to give an insight into health outcomes that may be associated with the physical environment. Explore multiple measures at once and adjust filters in real time."
     }
 ]
