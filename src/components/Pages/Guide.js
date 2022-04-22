097726f5b746f53bf7a3833c46a80df73cb415a5
Marynia <marynia.kolak@gmail.com>
2022-04-22 12:57:19 -0500

    update bold

--- src/components/Pages/Guide.js
+++ src/components/Pages/Guide.js
@@ -26,11 +26,11 @@ const Guide = () => {
 
                 <p>
                 The resource guide provides information about relevant websites, toolkits, and related items that relate to the Chicago environment at a local scale. 
-                We link to other maps, data, and resources about the Chicago environment from featured partners and a curated list of resources.
+                The Guide includes information from featured partners, as well as a curated list of other resources.
 
 
                 <br /><br />
-                Resources are <i> featured </i> if the author confirms the following:
+                Resources are <i> featured </i> if the original author confirms that is is:
                 <br /><br />
                 <li><b>Complete. </b> Each resource entry should include the resource name, website or media attachment, short description of resource & source, and featured image.
                                 </li>
@@ -45,7 +45,7 @@ const Guide = () => {
 
                 Add your resource guide by submitting the official <a href="">Resource Guide Form.</a> 
 
-                </p><br /><br />
+                </p><br /><br /><br /><br />
                     
                     
 
