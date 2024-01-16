f84e8d4d7403850620232664049e18fddc2498d4
Sara Lambert <lambert8@illinois.edu>
2024-01-16 13:34:26 -0600

    fix: Hashtag icon

--- src/components/Pages/IndexBuilder.js
+++ src/components/Pages/IndexBuilder.js
@@ -190,9 +190,11 @@ const WeightsPage = ({ selections, setSelections }) => {
     const IconProps = { color: WHITE, background: MAGENTA };
     const IndexBuilderStepIcon = ({ color, stepNumber, background }) => {
         switch(stepNumber) {
-            case 1: return (<div style={{ background, color, borderRadius: '20px', paddingTop: '5px', paddingBottom: '5px', paddingLeft: '7px', paddingRight: '3px', height: '24px', width: '24px' }}><FaMousePointer /></div>)
-            case 2: return (<div style={{ background, color, borderRadius: '20px', paddingTop: '5px', paddingBottom: '5px', paddingLeft: '7px', paddingRight: '3px', height: '24px', width: '24px' }}>
-                <svg className="fa-svg-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">{ /* !Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. */ }<path d="M181.3 32.4c17.4 2.9 29.2 19.4 26.3 36.8L197.8 128h95.1l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3s29.2 19.4 26.3 36.8L357.8 128H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H347.1L325.8 320H384c17.7 0 32 14.3 32 32s-14.3 32-32 32H315.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8l9.8-58.7H155.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8L90.2 384H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l21.3-128H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3zM187.1 192L165.8 320h95.1l21.3-128H187.1z"/></svg>
+            case 1: return (<div>
+                <FaMousePointer style={{ color, background,paddingLeft: '6px', paddingRight: '4px' }} />
+            </div>)
+            case 2: return (<div>
+                <FaHashtag style={{ color, background }} />
             </div>)
             default: return (<></>)
         }
@@ -221,7 +223,7 @@ const WeightsPage = ({ selections, setSelections }) => {
                                 </StepLabel>
                             </Step>
                             <Step key={'weights-step2'}>
-                                <StepLabel StepIconComponent={FaHashtag} StepIconProps={SecondIconProps}>
+                                <StepLabel StepIconComponent={IndexBuilderStepIcon} StepIconProps={SecondIconProps}>
                                     <Typography variant="subtitle">
                                         You can also change the weights by typing the percentage values in the input box
                                         next to the indicators.
