3517249175610dd6ac7b93c92fef9592c405dcb2
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2024-01-22 09:36:57 -0600

    add google translation widget to App.js to avoid page by page transition issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Suspense  } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import {Home, LoadingMessage, ErrorPage} from './components/';
-import { ThemeProvider, StyledEngineProvider, createTheme } from '@mui/material/styles';
-
-// import makeStyles from '@mui/styles/makeStyles';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, LoadingMessage, ErrorPage } from "./components/";
+import {
+	ThemeProvider,
+	StyledEngineProvider,
+	createTheme,
+} from "@mui/material/styles";
+import makeStyles from "@mui/styles/makeStyles";
 
 const theme = createTheme();
 
@@ -14,42 +16,84 @@ const theme = createTheme();
 //   }
 // });
 
-const Map = React.lazy(() => import('./components/Pages/Map'));
-const About = React.lazy(() => import('./components/Pages/About'));
-const Guide = React.lazy(() => import('./components/Pages/Guide'));
-const Contact = React.lazy(() => import('./components/Pages/Contact'));
-const Community = React.lazy(() => import('./components/Pages/Community'));
-const Data = React.lazy(() => import('./components/Pages/Data'));
+const Map = React.lazy(() => import("./components/Pages/Map"));
+const About = React.lazy(() => import("./components/Pages/About"));
+const Guide = React.lazy(() => import("./components/Pages/Guide"));
+const Contact = React.lazy(() => import("./components/Pages/Contact"));
+const Community = React.lazy(() => import("./components/Pages/Community"));
+const Data = React.lazy(() => import("./components/Pages/Data"));
+
+/** Google Translation Widget */
+const googleTranslateElementInit = () => {
+	new window.google.translate.TranslateElement(
+		{
+			pageLanguage: "en",
+			autoDisplay: false,
+		},
+		"google_translate_element"
+	);
+};
+const useStyles = makeStyles((theme) => ({
+	googleTranslateElement: {
+		position: "fixed",
+		bottom: "10px",
+		left: "10px",
+		zIndex: "1000",
+	},
+}));
 
 export default function App() {
+	const googleTranslateElement = useStyles();
+	React.useEffect(() => {
+		var addScript = document.createElement("script");
+		addScript.setAttribute(
+			"src",
+			"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+		);
+		document.body.appendChild(addScript);
+		window.googleTranslateElementInit = googleTranslateElementInit;
+	}, []);
 
-    return (
-      <Router basename={process.env.PUBLIC_URL}>
-      <div>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <Suspense fallback={<LoadingMessage />}>
-              <Switch>
-                <Route path="/" component={Home} exact/>
-                <Route path="/map" component={Map}/>
-                <Route path="/map.html" component={Map}/>
-                <Route path="/about" component={About}/>
-                <Route path="/about.html" component={About}/>
-                <Route path="/guide" component={Guide}/>
-                <Route path="/guide.html" component={Guide}/>
-                <Route path="/contact" component={Contact}/>
-                <Route path="/contact.html" component={Contact}/>
-                <Route path="/community" component={Community}/>
-                <Route path="/community.html" component={Community}/>              
-                <Route path="/data" component={Data}/>
-                <Route path="/data.html" component={Data}/>
-                <Route component={ErrorPage} />
-                <Route />
-            </Switch>
-          </Suspense>
-          </ThemeProvider>
-      </StyledEngineProvider>
-      </div> 
-    </Router>
-    );
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<div>
+				<div
+					id="google_translate_element"
+					className={googleTranslateElement.googleTranslateElement}
+				></div>
+				<StyledEngineProvider injectFirst>
+					<ThemeProvider theme={theme}>
+						<Suspense fallback={<LoadingMessage />}>
+							<Switch>
+								<Route path="/" component={Home} exact />
+								<Route path="/map" component={Map} />
+								<Route path="/map.html" component={Map} />
+								<Route path="/about" component={About} />
+								<Route path="/about.html" component={About} />
+								<Route path="/guide" component={Guide} />
+								<Route path="/guide.html" component={Guide} />
+								<Route path="/contact" component={Contact} />
+								<Route
+									path="/contact.html"
+									component={Contact}
+								/>
+								<Route
+									path="/community"
+									component={Community}
+								/>
+								<Route
+									path="/community.html"
+									component={Community}
+								/>
+								<Route path="/data" component={Data} />
+								<Route path="/data.html" component={Data} />
+								<Route component={ErrorPage} />
+								<Route />
+							</Switch>
+						</Suspense>
+					</ThemeProvider>
+				</StyledEngineProvider>
+			</div>
+		</Router>
+	);
 }
