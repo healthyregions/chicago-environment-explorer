55ec36b6d0ec4a40883222660c11750051a25aee
Sara Lambert <lambert8@illinois.edu>
2024-03-15 15:11:38 -0500

    fix: added custom icons for new variable categories

--- src/components/IndexBuilder/Indicators.js
+++ src/components/IndexBuilder/Indicators.js
@@ -29,6 +29,7 @@ const css = `
     }
 `
 
+
 const palegreen = '#B9D19F';
 
 const IndicatorsCategory = ({ variableName, selections, index }) => {
@@ -48,7 +49,7 @@ const IndicatorsCategory = ({ variableName, selections, index }) => {
             <img alt="" src={'/icons/index-builder/logo_tree.svg'} className={'filter-black'} />
         }
         {
-            (categoryName === 'Social') &&
+            (categoryName === 'Social' || categoryName === 'Social & Economic') &&
             <img alt="" src={'/icons/index-builder/logo_people.svg'} className={'filter-black'} />
         }
         {
@@ -60,8 +61,18 @@ const IndicatorsCategory = ({ variableName, selections, index }) => {
             <img alt="" src={'/icons/index-builder/logo_flood.svg'} className={'filter-black'} />
         }
         {
+            (categoryName === 'Heat Indicator') &&
+            /* Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.*/
+            <svg className={'filter-black'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M224 96c0-53-43-96-96-96S32 43 32 96v203.3C12.2 321.8 .2 351.1 0 383.3c-.4 70.3 56.8 128.2 127.1 128.7 .3 0 .6 0 .9 0 70.7 0 128-57.3 128-128 0-32.5-12.1-62.1-32-84.7V96zm-96 368l-.6 0c-43.9-.3-79.6-36.5-79.4-80.4 .2-34 19.3-51.7 32-66.1V96c0-26.5 21.5-48 48-48s48 21.5 48 48v221.5c12.6 14.3 32 32.2 32 66.5 0 44.1-35.9 80-80 80zm64-80c0 35.3-28.7 64-64 64s-64-28.7-64-64c0-23.7 12.9-44.3 32-55.4V96c0-17.7 14.3-32 32-32s32 14.3 32 32v232.6c19.1 11.1 32 31.7 32 55.4z"/></svg>
+
+        }
+        {
             (categoryName === 'Built Environment') &&
-            <img alt="" src={'/icons/index-builder/logo_traffic.svg'} className={'filter-black'} />
+            <img alt="" src={'/icons/index-builder/logo_traffic_light.svg'} className={'filter-black'} />
+        }
+        {
+            (categoryName === 'Housing') &&
+            <img alt="" src={'/icons/index-builder/logo_house.svg'} className={'filter-black'} />
         }
     </>
 
