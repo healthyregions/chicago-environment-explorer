58f557e38ab1a05f53c88518de56259c8d2b3dbb
Sara Lambert <lambert8@illinois.edu>
2025-06-27 14:06:15 -0500

    feat: filter based on tag querystring parameter

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import Grid from "@mui/material/Grid";
-import { NavLink } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {NavLink, useLocation} from "react-router-dom";
 
 import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
@@ -15,37 +14,75 @@ import {colors} from "../../config";
 import Divider from "@mui/material/Divider";
 
 const PostList = ({ posts, limit, hideHeader }) => {
-    return (
-       <>
-           {!hideHeader && <>
-               <Typography variant={'h1'}>News</Typography>
-               <Divider style={{ marginBottom: 0, color: colors.forest }} />
-           </>}
+  // Tags allow users to filter the list of Posts - collect all existing tags here
+  const [ allTags, setAllTags ] = useState([]);
 
-           <TableContainer>
-               <Table aria-label="simple table">
-                   <TableBody>
-                       {
-                           posts?.slice(0, limit)?.map(post => <TableRow
-                                   key={`news-slug-${post.slug}`}
-                                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                               >
-                               <TableCell component="th" scope="row">
-                                   <NavLink to={`/posts/${post.slug}`}>
-                                       <div item xs={12} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                                           <Typography className={'post-title'} variant={'h5'} >{post.title}</Typography>
-                                           <Typography variant={'subtitle2'} style={{ color: colors.darkgray }}>{post.date}</Typography>
-                                       </div>
-                                       <ReactMarkdown children={post.tagline} remarkPlugins={[remarkGfm]}></ReactMarkdown>
-                                   </NavLink>
-                               </TableCell>
-                           </TableRow>)
-                       }
-                   </TableBody>
-               </Table>
-           </TableContainer>
-       </>
-    );
+  // User can specify a list of tags a URL query string parameter
+  //   e.g. ?tags=events&tags=community  =>     tag = [ events, community ]
+  const { search } = useLocation();
+
+  // Filtered posts are stored here
+  const [ filteredPosts, setFilteredPosts ] = useState([]);
+
+  // Read existing tags from posts.json - use this to render the list of tags in the UI
+  // If new tags/categories are added later, they will automatically appear here
+  useEffect(() => {
+    const unique = posts?.map(post => post.tags)?.flat()?.filter((tag, i, arr) => arr?.indexOf(tag) === i);
+    console.log("Unique Tags found: ", unique);
+    setAllTags(unique);
+  }, [setAllTags, posts]);
+
+  // Filter for only posts that contain one or more of our querystring tags
+  useEffect(() => {
+    const searchParams = new URLSearchParams(search);
+    console.log("Search params: ", searchParams);
+    let tags = searchParams.get("tag");
+    if (typeof(tags) === 'string') {
+      tags = [tags]
+    }
+    console.log("Tags parsed: ", tags);
+
+    if (!search) {
+      console.log("No tags - showing all posts: ", posts);
+      setFilteredPosts(posts);
+      return
+    }
+    const filtered = posts?.filter(post => tags?.some(tag => post?.tags?.includes(tag)));
+    console.log("Filtered Posts found: ", filtered);
+    setFilteredPosts(filtered);
+  }, [setFilteredPosts, search, posts]);
+
+  return (
+    <>
+       {!hideHeader && <>
+           <Typography variant={'h1'}>News</Typography>
+           <Divider style={{ marginBottom: 0, color: colors.forest }} />
+       </>}
+
+       <TableContainer>
+           <Table aria-label="simple table">
+               <TableBody>
+                   {
+                     filteredPosts?.slice(0, limit)?.map(post => <TableRow
+                             key={`news-slug-${post.slug}`}
+                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                         >
+                         <TableCell component="th" scope="row">
+                             <NavLink to={`/posts/${post.slug}`}>
+                                 <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                                     <Typography className={'post-title'} variant={'h5'} >{post.title}</Typography>
+                                     <Typography variant={'subtitle2'} style={{ color: colors.darkgray }}>{post.date}</Typography>
+                                 </div>
+                                 <ReactMarkdown children={post.tagline} remarkPlugins={[remarkGfm]}></ReactMarkdown>
+                             </NavLink>
+                         </TableCell>
+                     </TableRow>)
+                   }
+               </TableBody>
+           </Table>
+       </TableContainer>
+     </>
+  );
 }
 
 export default PostList;
