fa3fab8fced4939f575507b3fbb1c836886faec3
Sara Lambert <lambert8@illinois.edu>
2025-06-27 15:41:20 -0500

    fix: PostList - UI component for filtering based on tags

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {NavLink, useLocation} from "react-router-dom";
+import {NavLink, useHistory, useLocation} from "react-router-dom";
 
 import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
@@ -12,6 +12,7 @@ import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import {colors} from "../../config";
 import Divider from "@mui/material/Divider";
+import {Chip} from "@mui/material";
 
 const PostList = ({ posts, limit, hideHeader }) => {
   // Tags allow users to filter the list of Posts - collect all existing tags here
@@ -20,6 +21,7 @@ const PostList = ({ posts, limit, hideHeader }) => {
   // User can specify a list of tags a URL query string parameter
   //   e.g. ?tags=events&tags=community  =>     tag = [ events, community ]
   const { search } = useLocation();
+  const [ requestedTags, setRequestedTags ] = useState([]);
 
   // Filtered posts are stored here
   const [ filteredPosts, setFilteredPosts ] = useState([]);
@@ -39,6 +41,7 @@ const PostList = ({ posts, limit, hideHeader }) => {
     const tagsRaw = searchParams.getAll("tag");
     const tags = typeof(tagsRaw) === 'string' ? [tagsRaw] : tagsRaw;
     console.log("Tags parsed: ", tags);
+    setRequestedTags(tags);
 
     if (!search) {
       console.log("No tags - showing all posts: ", posts);
@@ -48,10 +51,53 @@ const PostList = ({ posts, limit, hideHeader }) => {
     const filtered = posts?.filter(post => tags?.some(tag => post?.tags?.includes(tag)));
     console.log("Filtered Posts found: ", filtered);
     setFilteredPosts(filtered);
-  }, [setFilteredPosts, search, posts]);
+  }, [setFilteredPosts, setRequestedTags, search, posts]);
+
+  const history = useHistory();
+
+  // Given a tag, "select" this tag in the UI
+  // This will show as a URL query param and filter the list of posts
+  const selectTag = (tag) => {
+    if (!requestedTags?.includes(tag)) {
+      console.log('Adding tag: ', tag);
+      // Build up a query containing this tag and navigate to it
+      const newTags = requestedTags?.concat(tag);
+      const query = newTags?.map((tag) => `tag=${tag}`)?.join('&');
+      console.log(query);
+      const url = `/posts?${query}`;
+      console.log(url);
+      history.push(url);
+    }
+  }
+
+  // Given a tag, "deselect" this tag in the UI
+  // This will remove the URL query param and refilter the list of posts
+  const deselectTag = (tag) => {
+    if (requestedTags?.includes(tag)) {
+      console.log('Removing tag: ', tag);
+      // Build up a query without this tag and navigate to it
+      const query = requestedTags?.filter(t => tag !== t)?.map((tag) => `tag=${tag}`)?.join('&');
+      console.log(query);
+      const url = `/posts?${query}`;
+      console.log(url);
+      history.push(url);
+    }
+  }
 
   return (
     <>
+      <div>
+        {
+          allTags?.map((tag) =>
+            requestedTags?.includes(tag)
+              ? <Chip style={{marginRight: '3rem', cursor: 'pointer' }} label={`#${tag}`}
+                      variant="outlined" onClick={() => deselectTag(tag)} />
+              : <Chip style={{marginRight: '3rem', cursor: 'pointer' }} label={`#${tag}`}
+                      onClick={() => selectTag(tag)} />
+          )
+        }
+      </div>
+
        {!hideHeader && <>
            <Typography variant={'h1'}>News</Typography>
            <Divider style={{ marginBottom: 0, color: colors.forest }} />
@@ -72,6 +118,13 @@ const PostList = ({ posts, limit, hideHeader }) => {
                                      <Typography variant={'subtitle2'} style={{ color: colors.darkgray }}>{post.date}</Typography>
                                  </div>
                                  <ReactMarkdown children={post.tagline} remarkPlugins={[remarkGfm]}></ReactMarkdown>
+                                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                   {
+                                     post?.tags?.map((tag) =>
+                                       <NavLink to={`/posts?tag=${tag}`}>#{tag}</NavLink>
+                                     )
+                                   }
+                                 </div>
                              </NavLink>
                          </TableCell>
                      </TableRow>)
