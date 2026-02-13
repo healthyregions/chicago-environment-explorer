d967a193fe1b46a5546aaa0e1cc4ba18a01a6790
Sara Lambert <lambert8@illinois.edu>
2026-02-13 13:34:20 -0600

    fix: sync MapOverlayTooltip and GeoJSON for EPA Monitoring Sites

--- src/components/Map/MapOverlayTooltipContent.js
+++ src/components/Map/MapOverlayTooltipContent.js
@@ -4,42 +4,42 @@ import React from 'react';
 // The props passed to this component should contain an object of the hovered object (from deck, info.object by default),
 // as well as a reference to the overlay that was clicked
 const MapOverlayTooltipContent = ({content, overlay}) => {
-    const interpolatePopupTitle = (content, overlay) => {
-        let result = overlay?.popupTitle;
-        overlay?.popupTitle.match(/({[a-zA-Z_-]*})/g).forEach(match => {
-            const keyName = match.replace('{', '').replace('}', '');
-            result = result.replace(match, content[keyName]);
-        });
+  const interpolatePopupTitle = (content, overlay) => {
+    let result = overlay?.popupTitle;
+    overlay?.popupTitle.match(/({[a-zA-Z_-]*})/g).forEach(match => {
+      const keyName = match.replace('{', '').replace('}', '');
+      result = result.replace(match, content[keyName]);
+    });
 
-        return result;
-    };
+    return result;
+  };
 
-    const popupFields = JSON.parse(overlay?.popupContent);
-
-    const printContents = (popupFields, content) => {
-      return (
-        <>
-          {typeof content === 'boolean' && <>{printContents(popupFields, content ? 'Yes' : 'No')}</>}
-          {typeof content === 'string' && <tr key={'popup-content-' + popupFields}><td>{popupFields}</td><td>{content}</td></tr>}
-          {typeof content === 'object' && Object.keys(popupFields)?.map((keyName) =>
-              printContents(popupFields[keyName], content[keyName])
-          )}
-        </>
-      )
-    };
-
+  const popupFields = JSON.parse(overlay?.popupContent);
+  const printContents = (popupFields, content) => {
     return (
-        <>
-            <h2>{interpolatePopupTitle(content, overlay)}</h2>
-            {content && <div style={{overflowY:'scroll', height:'150px'}}><span><table>
+      <>
+        {typeof content === 'boolean' && <>{printContents(popupFields, content ? 'Yes' : 'No')}</>}
+        {typeof content === 'string' && <tr key={'popup-content-' + popupFields}><td>{popupFields}</td><td>{content}</td></tr>}
+        {typeof content === 'object' && content.sort && printContents(popupFields, content.join(' & '))}
+        {typeof content === 'object' && !content.sort && Object.keys(popupFields)?.map((keyName) =>
+          printContents(popupFields[keyName], content[keyName])
+        )}
+      </>
+    )
+  };
+
+  return (
+    <>
+      <h2>{interpolatePopupTitle(content, overlay)}</h2>
+      {content && <div style={{overflowY:'scroll', height:'150px'}}><span><table>
                 <tbody>
                     {printContents(popupFields, content)}
                 </tbody>
             </table>
 
             </span></div>}
-        </>
-    )
+    </>
+  )
 }
 
 export default MapOverlayTooltipContent;
